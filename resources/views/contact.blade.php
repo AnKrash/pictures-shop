0b43ef301f43cb81c0a57281c149914016d9358a
Andrey <ankrash@ukr.net>
2021-05-13 22:16:30 +0300

    new

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,31 @@
 @section('title-block')Страница Контактов@endsection
 
 @section('content')
-<h1>Страница Контактов</h1>
+    <h1>Страница Контактов</h1>
+
+    <form action={{route('contact-form')}} method="post">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Введите имя</label>git
+            <input type="text" name="name"  placeholder="Введите имя" id="name"
+                   class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="name">Введите email </label>
+            <input type="text" name="email" placeholder="Введите email" id="email"
+                   class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="name">Тема сообщения</label>
+            <input type="text" name="subject"  placeholder="Subject" id="subject"
+                   class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Cообщение</label>
+            <textarea name="message" id="message" placeholder="Введите сообщение"
+                      class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Обновить</button>
+    </form>
 @endsection
-
-
