adcea61ed606f90fc676acc6ec805b44b55cee5d
ankrash <ankrash@ukr.net>
2021-06-30 13:17:25 +0300

    Frontend made without carusel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,8 @@
 
 
 @section('content')
+    <div class="container container-fluid3 p-5 mx-xs-0    px-xs-0">
+        <div class="p-5 mx-xs-0  mx-sm-0  px-xs-0">
     <h1>Про нас</h1>
     <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc at scelerisque
         libero. Aliquam at arcu mauris. Vivamus dignissim fringilla quam non auctor. Etiam varius nec eros vel feugiat.
@@ -16,8 +18,8 @@
         sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer placerat, urna vel molestie
         mattis, eros nisi vehicula leo, non iaculis sapien elit id dolor. Nulla nec ante mollis, ultrices felis at,
         sagittis felis.</p>
+    </div>
 
-    <div class="container container-fluid3 p-5 mx-xs-0    px-xs-0">
         <div class="p-5 mx-xs-0  mx-sm-0  px-xs-0">
             <h3 class="HeadItems">MEET OUR BEAUTIFUL TEAM!</h3>
             <hr class="hr3">
@@ -26,7 +28,7 @@
 
             <div class="row text-center">
                 <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
-                    <img src="images/Ann.png" alt="" height="140" width="140"/>
+                    <img src="Img-main/Ann.png" alt="" height="140" width="140"/>
                     <h6>ANN HATHAWAY</h6>
                     <p class="p3">CEO/Marketing Guru</p>
                     <p class=" d-none d-sm-block employee-descr">Interdum et malesuada fames ac ante ipsum primis in faucibus.
@@ -43,7 +45,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
-                    <img src="images/upton.png" alt="" height="140" width="140"/>
+                    <img src="Img-main/upton.png" alt="" height="140" width="140"/>
                     <h6>KATE UPTON</h6>
                     <p class="p3">Creative Director</p>
                     <p class=" d-none d-sm-block employee-descr">Fusce lobortis metus vitae erat condimentum, vitae efficitur enim
@@ -58,7 +60,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
-                    <img src="images/olivya.png" alt="" height="140" width="140"/>
+                    <img src="Img-main/olivya.png" alt="" height="140" width="140"/>
                     <h6>OLIVIA WILDE</h6>
                     <p class="p3">Lead Designer</p>
                     <p class=" d-none d-sm-block employee-descr"> Aliquam quis aliquam mauris. Suspendisse mauris arcu, ultricies eu
@@ -74,7 +76,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
-                    <img src="images/ashly.png" alt="" height="140" width="140"/>
+                    <img src="Img-main/ashly.png" alt="" height="140" width="140"/>
                     <h6>ASHLEY GREENE</h6>
                     <p class="p3">SEO/Developer</p>
                     <p class="d-none d-sm-block employee-descr"> Phasellus pulvinar sagittis volutpat. Vestibulum ante ipsum primis in
@@ -88,10 +90,16 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            </div>
+            <div class="p-5 mx-xs-0  mx-sm-0  px-xs-0">
+                <h3 class="HeadItems">If You want to be a member club-register now!</h3>
+                <a class="me-3 py-2 px-3 text-dark  " href="{{route('contact')}}">
+                    <h4>Register!</h4>
+                </a>
 
             </div>
 
-        </div>
 @endsection
 
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,66 @@
 @section('title-block')Страница Контактов@endsection
 
 @section('content')
+    <div class="container container-fluid6 p-5 mx-xs-0    px-xs-0">
+        <div class="p-5 mx-xs-0    px-xs-0 mx-xs-0  mx-sm-0  px-sm-0">
+            <h3 class="HeadItems HeadItems6">WHAT PEOPLE SAY ABOUT US</h3>
+            <hr class="hr6">
+            <p class="text-items text-items6"> Our clients love us! </p>
+        </div>
+        <div class=" row  justify-content-center mt-sm-3   py-sm-5 py-md-5 my-md-5 row6"
+        >
+            <div class="col-auto">
+                <img src="images/1933_oooo.plus.png" alt="" height="70" width="70"/>
+            </div>
+            <div class="col ">
+                <p style="font-style: italic"> Suspendisse mauris arcu</p>
+                <p class=" d-none d-sm-block">ultricies eu dictum quis, luctus nec dui. Sed
+                    lobortis at diam nec
+                    venenatis.</p>
+                <h6>CHANEL IMAN</h6>
+                <p class="p6">Ceo of Pinterest</p>
+            </div>
+            <div class=" col-auto">
+
+                <img src="images/1706_oooo.plus.png" alt="" height="70" width="70"/></div>
+            <div class="col">
+                <p style="font-style: italic">Fusce a sapien id diam</p>
+                <p class=" d-none d-sm-block">tincidunt pharetra ac vitae nunc. Aliquam a dui sed
+                    elit posuere bibendum eu posuere risus. </p>
+                <h6>ADRIANA LIMA</h6>
+                <p class="p6">Founder of Instagram</p>
+            </div>
+
+        </div>
+        <div class=" row  justify-content-center row6">
+            <div class="col-auto">
+                <img src="images/1991_oooo.plus.png" alt="" height="70" width="70"/></div>
+            <div class="col">
+                <p style="font-style: italic"> Nullam imperdiet</p>
+                <p class=" d-none d-sm-block">urna sodales leo bibendum tristique. Praesent vehicula
+                    finibus tempus. Integer at est lacinia erat dignissim faucibus quis in massa.
+                </p>
+                <h6>ANNE HATHAWAY</h6>
+                <p class="p6">Lead Designer at Behance</p>
+            </div>
+            <div class="col-auto">
+
+                <img src="images/1271_oooo.plus.png" alt="" height="70" width="70"/></div>
+            <div class="col">
+                <p style="font-style: italic"> Curabitur bibendum</p>
+                <p class=" d-none d-sm-block">nunc et tempor suscipit, tellus risus sodales mi, non
+                    imperdiet tellus purus at neque. Fusce lobortis metus vitae erat condimentum </p>
+                <h6>EMMA STONE</h6>
+                <p class="p6">Co-foundef of Shazam</p>
+            </div>
+
+        </div>
+    </div>
+<div> <h3 >If You want to be a member club-register now!</h3>
+    <h3 >If You want to make a message-register now!</h3>
+    <h3 >If You want to have a message from latest news about art-register now!</h3>
+</div>
+<br><br>
     <h1>Страница Контактов</h1>
 
     <form action={{route('contact-form')}} method="post">
